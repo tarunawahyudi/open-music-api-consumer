60a31b57c86449cab0fc572cd738ec70d838d486
Taruna Wahyudi <wahyuditaruna97@gmail.com>
2024-04-13 03:36:00 +0700

    update

---
+++ PlaylistsService.js
@@ -0,0 +1,39 @@
+const { Pool } = require('pg');
+
+
+class PlaylistsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async getSongsFromPlaylist(playlistId) {
+    const query = {
+      text: 'SELECT * FROM p.id p.name FROM playlists p LEFT JOIN users u ON p.owner=u.id WHERE p.id=$1',
+      values: [playlistId],
+    }
+
+    const resultPlaylist = await this._pool.query(query);
+
+    const playlist = resultPlaylist.rows[0];
+
+    const songsQuery = {
+      text: 'SELECT * FROM playlist_songs ps INNER JOIN songs s ON s.id=ps.song_id WHERE ps.playlist_id = $1',
+      values: [playlist.id],
+    };
+
+    const resultSongs = await this._pool.query(songsQuery);
+    const songs = resultSongs.rows;
+
+    return {
+      id: playlist.id,
+      name: playlist.name,
+      songs: songs.map((song) => ({
+        id: song.id,
+        title: song.title,
+        performer: song.performer,
+      })),
+    };
+  }
+}
+
+module.exports = PlaylistsService;
